4937838d50de347b270aad376dcc305a36dea5e3
Alex Bogushenko <bogushenkoas@ukr.net>
2023-05-15 11:42:07 +0300

    2 commit

--- main.cpp
+++ main.cpp
@@ -6,38 +6,74 @@ using namespace std;
 
 bool check(string word_1, string word_2)
 {
+    map <char, int> charcount;
+    int n = 0;
+    int number = 1;
+    auto it = charcount.find(word_1[n]);
+    
     if(word_1.length() != word_2.length())
-    {
-        return 0;
-    }
-    int a = 0;
-
-    for(int i = 0; i < word_1.length(); i ++)
       {
-        for(int n = 0; n < word_2.length(); n ++)
-        {
-            if(word_1[i] == word_2[n])
-            {
-                a = 1;
-            }
-           
-        }
-        if(a == 0)
-        {
-          return 0;
-        }
-
-        a = 0;
+        return 0;
 
       }
+     
 
-  return 1;
+      for( n = 0; n < word_1.length(); n ++)
+        {
+          it = charcount.find(word_1[n]);
+          
+          if(it == charcount.end())
+            {
+              charcount.insert(pair <char, int>(word_1[n], number));
+            }
+          
+            else
+              {
+              it->second ++;
+
+              }
+         
+         }
+
+         for(n = 0; n < word_2.length(); n ++)
+          {
+             it = charcount.find(word_2[n]);
+              if(it == charcount.end())
+                {
+                 return 0;
+                }
+                else
+                  {
+                    int a = it->second;
+
+                    if(a > 1)
+                     {
+                      it->second --;
+                     }
+                      else
+                      {
+                        charcount.erase(word_2[n]);
+                      }
+
+                  }                
+            }
+
+            if(charcount.empty())
+            {
+              return 1;
+            }
+
+           
+
+      
+
+  return 0;
 }
 
 int main() {
-    string word_1 = "лекарство";
-    string word_2 = "стекловар";
-
+    string word_1 = "lekarstvo";
+    string word_2 = "steklovar";
+    
     bool result = check(word_1, word_2);
 
     if(result){
@@ -49,6 +85,10 @@ int main() {
          cout << "is not an anagram";
 
     }
+
+
+  
+
 
 
     return 0;
